46bce4fd7a5cf556c517c2f8c0d0566c5c9deea0
= <=>
2023-05-21 00:00:20 -0700

    Set up event listeners to respond to user events Use fetch() to make a GET request, then render the returned toys to the DOM Use fetch() to make a POST request to create a new toy, then add it to the DOM Use fetch() to make a PATCH request that updates an existing toy, then render the updated information to the DOM

--- src/index.js
+++ src/index.js
@@ -12,4 +12,76 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  // Fetch Andy's Toys
+  fetch('http://localhost:3000/toys')
+    .then(response => response.json())
+    .then(toys => {
+      let toysContainer = document.getElementById("toy-collection");
+      toys.forEach(toy => {
+        toysContainer.innerHTML += `
+          <div class="card">
+            <h2>${toy.name}</h2>
+            <img src="${toy.image}" class="toy-avatar" />
+            <p>${toy.likes} Likes </p>
+            <button class="like-btn" id="${toy.id}">Like ❤️</button>
+          </div>
+        `;
+      });
+    });
+
+  // Add a New Toy
+  let toyForm = document.querySelector(".add-toy-form");
+  toyForm.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    fetch('http://localhost:3000/toys', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        "name": toyForm.name.value,
+        "image": toyForm.image.value,
+        "likes": 0
+      })
+    })
+    .then(response => response.json())
+    .then(toy => {
+      let toysContainer = document.getElementById("toy-collection");
+      toysContainer.innerHTML += `
+        <div class="card">
+          <h2>${toy.name}</h2>
+          <img src="${toy.image}" class="toy-avatar" />
+          <p>${toy.likes} Likes</p>
+          <button class="like-btn" id="${toy.id}">Like ❤️</button>
+        </div>
+      `;
+    });
+  });
+
+  // Increase a Toy's Likes
+  let toysContainer = document.getElementById("toy-collection");
+  toysContainer.addEventListener("click", function(e) {
+    if(e.target.className === "like-btn") {
+      let currentLikes = parseInt(e.target.previousElementSibling.innerText);
+      let newLikes = currentLikes + 1;
+
+      fetch(`http://localhost:3000/toys/${e.target.id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify({
+          "likes": newLikes
+        })
+      })
+      .then(response => response.json())
+      .then(toy => {
+        e.target.previousElementSibling.innerText = `${toy.likes} Likes`;
+      });
+    }
+  });
 });
